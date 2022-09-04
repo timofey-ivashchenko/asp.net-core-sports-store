8ceb0a271a3a738720f19af30ebd2b31ee44317d
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-09-04 19:21:21 +0300

    Настройка EF Core.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(
+	options => options.UseSqlServer(
+		builder.Configuration["ConnectionStrings:Main"]));
 
 var app = builder.Build();
 
