e36f31665fb2ac126b1fa6037fd22045fe169fbb
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-09-04 23:11:17 +0300

    Класс начальной миграции (часть 2).

--- SportsStore/Migrations/20220904175421_Initial.cs
+++ SportsStore/Migrations/20220904175421_Initial.cs
@@ -1,11 +1,58 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using SportsStore.Models;
 
 #nullable disable
 
 namespace SportsStore.Migrations;
 
+[DbContext(typeof(StoreDbContext))]
+[Migration("20220904175421_Initial")]
 public class Initial : Migration
 {
+	protected override void BuildTargetModel(ModelBuilder modelBuilder)
+	{
+#pragma warning disable 612, 618
+
+		modelBuilder
+			.HasAnnotation("ProductVersion", "6.0.8")
+			.HasAnnotation("Relational:MaxIdentifierLength", "128")
+			.UseIdentityColumns(1L, 1)
+			.Entity<Product>(builder =>
+			{
+				builder
+					.Property<long?>("Id")
+					.HasColumnType("bigint")
+					.UseIdentityColumn(1L, 1)
+					.ValueGeneratedOnAdd();
+
+				builder
+					.Property<string>("Name")
+					.HasColumnType("nvarchar(max)")
+					.IsRequired();
+
+				builder
+					.Property<string>("Description")
+					.HasColumnType("nvarchar(max)")
+					.IsRequired();
+
+				builder
+					.Property<string>("Category")
+					.HasColumnType("nvarchar(max)")
+					.IsRequired();
+
+				builder
+					.Property<decimal>("Price")
+					.HasColumnType("decimal(8,2)")
+					.IsRequired();
+
+				builder.ToTable("Products").HasKey("Id");
+			});
+
+#pragma warning restore 612, 618
+	}
+
 	protected override void Down(MigrationBuilder migrationBuilder) =>
 		migrationBuilder.DropTable(name: "Products");
 
