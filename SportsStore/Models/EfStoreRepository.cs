269e05db08ea2007c285098104aa9f07dfc1a791
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-09-04 20:45:06 +0300

    Добавлен репозиторий.

---
+++ SportsStore/Models/EfStoreRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStore.Models;
+
+public class EfStoreRepository : IStoreRepository
+{
+	readonly StoreDbContext _context;
+
+	public EfStoreRepository(StoreDbContext context) =>
+		_context = context;
+
+	public IQueryable<Product> Products => _context.Products;
+}
