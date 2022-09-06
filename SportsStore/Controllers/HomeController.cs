0d2d7f4365d5e07d0a2ce1e08c76098a4b5be4ef
Tim Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-09-06 09:13:55 +0300

    Передача репозитория в HomeController.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers;
 
 public class HomeController : Controller
 {
-	public IActionResult Index() => View();
+	readonly IStoreRepository _repository;
+
+	public HomeController(IStoreRepository repository) =>
+		_repository = repository;
+
+	public IActionResult Index() => View(_repository.Products);
 }
