9a7e0f0506246fb476e3c38b31f471c0f79d133c
Tim Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-09-06 16:58:51 +0300

    Постраничная выборка продуктов.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -5,10 +5,22 @@ namespace SportsStore.Controllers;
 
 public class HomeController : Controller
 {
+	const int PageSize = 4;
+
 	readonly IStoreRepository _repository;
 
 	public HomeController(IStoreRepository repository) =>
 		_repository = repository;
 
-	public IActionResult Index() => View(_repository.Products);
+	public IActionResult Index(int page = 1)
+	{
+		if (page < 1) page = 1;
+
+		var products = _repository.Products
+			.OrderBy(x => x.Id)
+			.Skip((page - 1) * PageSize)
+			.Take(PageSize);
+
+		return View(products);
+	}
 }
