fac2a4ebe777044d340920d231f271ce3df6398f
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-09-19 22:31:15 +0300

    Выборка категорий товаров + тест.

---
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+using SportsStore.Components;
+using SportsStore.Models;
+
+namespace SportsStore.Tests;
+
+public class NavigationMenuViewComponentTests
+{
+	[Fact]
+	public void CanSelectCategories()
+	{
+		// Arrange.
+
+		var repository = new Mock<IStoreRepository>();
+
+		repository.Setup(x => x.Products).Returns(
+			new Product[]
+			{
+				new() { Name = "P1", Category = "C3"},
+				new() { Name = "P2", Category = "C2"},
+				new() { Name = "P3", Category = "C1"},
+				new() { Name = "P4", Category = "C3"},
+				new() { Name = "P5", Category = "C4"},
+				new() { Name = "P6", Category = "C5"},
+				new() { Name = "P7", Category = "C1"},
+				new() { Name = "P8", Category = "C2"},
+				new() { Name = "P9", Category = "C5"}
+			}.AsQueryable());
+
+		var component = new NavigationMenuViewComponent(repository.Object);
+
+		// Act.
+
+		var result = component.Invoke() as ViewViewComponentResult;
+		var categories = result?.ViewData?.Model is IEnumerable<string> model
+			? model.ToList() : new List<string>();
+
+		// Assert.
+
+		Assert.True(categories.SequenceEqual(
+			new[] { "C1", "C2", "C3", "C4", "C5" }));
+	}
+}
--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Components;
 
 public class NavigationMenuViewComponent : ViewComponent
 {
-	public string Invoke() => "Hello from the Navigation Menu View Component!";
+	readonly IStoreRepository _repository;
+
+	public NavigationMenuViewComponent(IStoreRepository repository) =>
+		_repository = repository;
+
+	public IViewComponentResult Invoke() =>
+		View(_repository.Products
+			.Select(x => x.Category)
+			.Distinct()
+			.OrderBy(x => x));
 }
