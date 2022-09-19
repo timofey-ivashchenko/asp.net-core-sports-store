178a2172d34870e1c819f9c807d12761282f0641
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-09-19 21:14:15 +0300

    Меню навигации добавлено в главный шаблон.

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components;
+
+public class NavigationMenuViewComponent : ViewComponent
+{
+	public string Invoke() => "Hello from the Navigation Menu View Component!";
+}
