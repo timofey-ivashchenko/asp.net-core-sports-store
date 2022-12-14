275621ad7aad6c3d1e34085c598dccff748a759e
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-14 23:20:15 +0200

    Косметика.

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -4,8 +4,7 @@ public class EFStoreRepository : IStoreRepository
 {
 	readonly StoreDbContext _context;
 
-	public EFStoreRepository(StoreDbContext context) =>
-		_context = context;
+	public EFStoreRepository(StoreDbContext context) => _context = context;
 
 	public IQueryable<Product> Products => _context.Products;
 }
