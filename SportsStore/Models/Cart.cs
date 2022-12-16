fd40728d277a92f9e41a3cdb4b8f6879081f3b10
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-16 02:35:32 +0200

    Добавлен класс Cart.

---
+++ SportsStore/Models/Cart.cs
@@ -0,0 +1,29 @@
+﻿namespace SportsStore.Models;
+
+public class Cart
+{
+	public List<CartLine> Lines { get; } = new();
+
+	public void AddItem(Product product, int quantity)
+	{
+		var line = Lines.FirstOrDefault(
+			x => x.Product.ProductID == product.ProductID);
+
+		if (line is null)
+		{
+			Lines.Add(new() { Product = product, Quantity = quantity });
+		}
+		else
+		{
+			line.Quantity += quantity;
+		}
+	}
+
+	public void Clear() => Lines.Clear();
+
+	public decimal ComputeTotalValue() =>
+		Lines.Sum(x => x.Product.Price * x.Quantity);
+
+	public void RemoveLine(Product product) =>
+		Lines.RemoveAll(x => x.Product.ProductID == product.ProductID);
+}
