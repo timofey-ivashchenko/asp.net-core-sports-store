5272449c0132e1526d14cda2ecf5197d1d06b08c
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-16 02:11:44 +0200

    Добавлен класс CartLine.

---
+++ SportsStore/Models/CartLine.cs
@@ -0,0 +1,10 @@
+namespace SportsStore.Models;
+
+public class CartLine
+{
+	public int CartLineID { get; set; }
+
+	public Product Product { get; set; } = new();
+
+	public int Quantity { get; set; }
+}
