882c1b44f00526b0c6f1366989edfb276898abcb
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-18 22:19:52 +0200

    Методы расширения ISession.

---
+++ SportsStore/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace SportsStore.Infrastructure;
+
+public static class SessionExtensions
+{
+	public static T? GetJson<T>(this ISession session, string key)
+	{
+		var sessionData = session.GetString(key);
+
+		return sessionData is not null
+			? JsonSerializer.Deserialize<T>(sessionData) : default;
+	}
+
+	public static void SetJson(
+		this ISession session, string key, object value) =>
+		session.SetString(key, JsonSerializer.Serialize(value));
+}
