9955508a5463c7e6a3fcbfb4db09b4f1f49be3d2
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-19 22:54:58 +0200

    Тест CartModel.OnPost.

--- SportsStore.Tests/CartPageTests.cs
+++ SportsStore.Tests/CartPageTests.cs
@@ -64,4 +64,61 @@ public class CartPageTests
 		Assert.Equal(2, cartModel.Cart.Lines.Count);
 		Assert.Equal("myUrl", cartModel.ReturnUrl);
 	}
+
+	[Fact]
+	public void CanUpdateCart()
+	{
+		// Arrange.
+
+		var product1 = new Product
+		{
+			ProductID = 1,
+			Name = "Дом в Таромском",
+			Description = "Мой будущий дом.",
+			Category = "Недвижимость",
+			Price = 300000
+		};
+
+		var repository = new Mock<IStoreRepository>();
+		repository.Setup(x => x.Products).Returns(
+			new[] { product1 }.AsQueryable());
+
+		var cart = new Cart();
+
+		var session = new Mock<ISession>();
+		session.Setup(x => x.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+			.Callback<string, byte[]>((_, value) =>
+			{
+				cart = JsonSerializer.Deserialize<Cart>(Encoding.UTF8.GetString(value));
+			});
+
+		var context = new Mock<HttpContext>();
+		context.Setup(x => x.Session).Returns(session.Object);
+
+		// Act.
+
+		var cartModel = new CartModel(repository.Object)
+		{
+			PageContext = new(new()
+			{
+				ActionDescriptor = new PageActionDescriptor(),
+				HttpContext = context.Object,
+				RouteData = new()
+			})
+		};
+
+		cartModel.OnPost(1, "myUrl");
+
+		// Assert.
+
+		Assert.Single(cart.Lines);
+		var line = cart.Lines.First();
+		var product2 = line.Product;
+		Assert.Equal(product1.ProductID, product2.ProductID);
+		Assert.Equal(product1.Name, product2.Name);
+		Assert.Equal(product1.Description, product2.Description);
+		Assert.Equal(product1.Category, product2.Category);
+		Assert.Equal(product1.Price, product2.Price);
+		Assert.Equal(1, line.Quantity);
+	}
 }
