3318100ed856a16f9ecbecf49f8a8c5d8304958e
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-21 14:05:28 +0200

    Удаление товара из корзины.

--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -29,4 +29,12 @@ public class CartModel : PageModel
 
 		return RedirectToPage(new { returnUrl });
 	}
+
+	public IActionResult OnPostRemove(long productId, string returnUrl)
+	{
+		Cart.RemoveLine(Cart.Lines.First(
+			x => x.Product.ProductID == productId).Product);
+
+		return RedirectToPage(new { returnUrl });
+	}
 }
