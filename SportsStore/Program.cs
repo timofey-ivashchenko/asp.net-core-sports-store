c516a3c976afa0cfecd5ca7caa181eff2abcab13
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-21 08:17:06 +0200

    Регистрация сервиса.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -24,6 +24,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 
+builder.Services.AddScoped(SessionCart.GetCart);
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
