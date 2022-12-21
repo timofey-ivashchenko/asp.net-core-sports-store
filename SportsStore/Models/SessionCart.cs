ee01846820346a8b3c8738ddf5a9a2d17e478e46
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-21 06:36:28 +0200

    Доработка класса SessionCart.

--- SportsStore/Models/SessionCart.cs
+++ SportsStore/Models/SessionCart.cs
@@ -1,38 +1,36 @@
 ﻿using SportsStore.Infrastructure;
-using System.Text.Json.Serialization;
 
 namespace SportsStore.Models;
 
 public class SessionCart : Cart
 {
 	const string SessionKey = "cart";
-	
-	[JsonIgnore]
-	public ISession? Session { get; set; }
+
+	ISession? _session;
 	
 	public static Cart GetCart(IServiceProvider services)
 	{
 		var session = services.GetRequiredService<IHttpContextAccessor>().HttpContext?.Session;
 		var cart = session?.GetObject<SessionCart>(SessionKey) ?? new();
-		cart.Session = session;
+		cart._session = session;
 		return cart;
 	}
 
 	public override void AddItem(Product product, int quantity)
 	{
 		base.AddItem(product, quantity);
-		Session?.SetObject(SessionKey, this);
+		_session?.SetObject(SessionKey, this);
 	}
 
 	public override void Clear()
 	{
 		base.Clear();
-		Session?.Remove(SessionKey);
+		_session?.Remove(SessionKey);
 	}
 
 	public override void RemoveLine(Product product)
 	{
 		base.RemoveLine(product);
-		Session?.SetObject(SessionKey, this);
+		_session?.SetObject(SessionKey, this);
 	}
 }
