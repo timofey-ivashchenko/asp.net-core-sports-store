8d0296e4dc3aaa5cce68cfa5af2198664cfc86b2
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-21 17:24:34 +0200

    Добавлен компонент сводки корзины.

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components;
+
+public class CartSummaryViewComponent : ViewComponent
+{
+	readonly Cart _cart;
+
+	public CartSummaryViewComponent(Cart cart) => _cart = cart;
+
+	public IViewComponentResult Invoke() => View(_cart);
+}
