042b3db715ba381a59da280a1c41eafaef52fe47
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-24 09:46:53 +0200

    Добавлено свойство StoreDbContext.Orders.

--- SportsStore/Models/StoreDbContext.cs
+++ SportsStore/Models/StoreDbContext.cs
@@ -6,5 +6,7 @@ public class StoreDbContext : DbContext
 {
 	public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) {}
 
+	public DbSet<Order> Orders => Set<Order>();
+
 	public DbSet<Product> Products => Set<Product>();
 }
