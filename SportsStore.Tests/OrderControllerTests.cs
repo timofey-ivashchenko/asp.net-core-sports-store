7c660c5d7223fb4c5c25b30b6e194ae1da0b2ab5
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2022-12-28 23:09:31 +0200

    Тест: нельзя оформить заказ с пустой корзиной.

---
+++ SportsStore.Tests/OrderControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Controllers;
+using SportsStore.Models;
+
+namespace SportsStore.Tests;
+
+public class OrderControllerTests
+{
+	[Fact]
+	public void CannotCheckoutEmptyCart()
+	{
+		// Arrange.
+
+		var repository = new Mock<IOrderRepository>();
+		var cart = new Cart();
+		var controller = new OrderController(repository.Object, cart);
+		var order = new Order();
+
+		// Act.
+
+		var result = controller.Checkout(order) as ViewResult;
+
+		// Assert.
+
+		// Проверяем, что заказ не был сохранён.
+		repository.Verify(x => x.SaveOrder(It.IsAny<Order>()), Times.Never);
+
+		// Проверяем, что метод возвращает представление по умолчанию.
+		Assert.True(string.IsNullOrEmpty(result?.ViewName));
+
+		// Проверяем, что в представление передаётся недействительная модель.
+		Assert.False(result?.ViewData.ModelState.IsValid);
+	}
+}
